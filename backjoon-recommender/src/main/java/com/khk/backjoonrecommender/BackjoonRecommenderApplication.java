9ea76632466bd730b7edf8b22d0b2b723f89264e
xi_jjun <rlawowns97@naver.com>
2022-06-26 23:58:04 +0900

    추가: server init code

---
+++ backjoon-recommender/src/main/java/com/khk/backjoonrecommender/BackjoonRecommenderApplication.java
@@ -0,0 +1,13 @@
+package com.khk.backjoonrecommender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackjoonRecommenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackjoonRecommenderApplication.class, args);
+	}
+
+}
