ebbf190e0a3b2f66414312729f25e5718ad8cda0
nyk <nykoh2001@gmail.com>
2022-06-27 21:10:00 +0900

    [추가] community 페이지들에 사용되는 공통 스타일 요소

---
+++ front/src/Pages/community/Community.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+width: 100%;
+display: flex;
+flex-direction: column;
+justify-content: space-between;
+box-sizing: border-box;
+padding: 20px 0;
+`
+export const Input = styled.input`
+width: 100%;
+height: 40px;
+box-sizing: border-box;
+border: solid 1px #bbb;
+padding: 0 24px;
+margin: 5px 0;
+`
+
+export const InputSubContainer = styled.div`
+width: 100%;
+display: flex;
+align-items: center;
+`
