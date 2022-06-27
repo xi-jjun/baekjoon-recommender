0aa639463af3df95926bca9ae383ffa797f3325a
nyk <nykoh2001@gmail.com>
2022-06-27 21:13:50 +0900

    [추가] App.js: 라우팅

---
+++ front/src/shared/App.js
@@ -0,0 +1,25 @@
+import { React } from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../Pages/community/Login/Login';
+import SignUp from '../Pages/community/SignUp/SignUp';
+import Recommend from '../Pages/main/Recommend/Recommend';
+import Rival from "../Pages/main/Rival/Rival";
+import Donate from "../Pages/main/Donate/Donate";
+import Solved from "../Pages/main/Solved/Solved";
+import MyPage from '../Pages/main/MyPage/MyPage';
+
+function App() {
+    return (
+        <Routes>
+            <Route path="/" element={<Login />} />
+            <Route path="/signUp" element={<SignUp />} />
+            <Route path="/recommend" element={<Recommend />} />
+            <Route path="/solved" element={<Solved />} />
+            <Route path="/rival" element={<Rival />} />
+            <Route path="/donate" element={<Donate />} />
+            <Route path="/myPage" element={<MyPage />} />
+        </Routes>
+    );
+}
+
+export default App;
