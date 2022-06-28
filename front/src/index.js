c9084e2d770e8d894b715e8e394bbae6b030d0c2
nyk <nykoh2001@gmail.com>
2022-06-28 11:36:36 +0900

    [수정] 불필요한 링크 제거

--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Root from "./client/Root";
-import Login from './Pages/community/Login/Login';
-import SignUp from './Pages/community/SignUp/SignUp';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
