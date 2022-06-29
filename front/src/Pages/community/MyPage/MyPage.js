e64cee2eeb16a52279395566fb712c2921126c05
nyk <nykoh2001@gmail.com>
2022-06-29 23:17:26 +0900

    [추가] 마이페이지

--- front/src/Pages/community/MyPage/MyPage.js
+++ front/src/Pages/community/MyPage/MyPage.js
@@ -1,8 +1,41 @@
 import Header from "../../../Components/Header";
+import * as Default from "../../../Default";
+import * as Community from "../Community";
+import * as Styled from "./Styled";
+import CommunityButton from "../../../Components/CommunityButton";
+import Toggle from "../../../Components/Toggle";
+import Button from "../../../Components/Button";
 
 const MyPage = () => {
     return (
-        <Header />
+        <div>
+            <Header />
+            <Styled.Container>
+                <Community.InfoContainer>
+                    <Community.InfoContainerLabel>Membership Info</Community.InfoContainerLabel>
+                    <Community.InfoSubContainer>
+                        <Community.InfoLabel>ID</Community.InfoLabel>
+                        <Styled.Info>baekjoonRecommender</Styled.Info>
+                    </Community.InfoSubContainer>
+                    <Community.InfoSubContainer>
+                        <Community.InfoLabel>PW</Community.InfoLabel>
+                        <Community.Input placeholder="기존 pw" />
+                    </Community.InfoSubContainer>
+                    <Community.InfoSubContainer>
+                        <Community.InfoLabel></Community.InfoLabel>
+                        <Community.ShortInput placeholder="새로운 pw" />
+                        <CommunityButton typo="수정" />
+                    </Community.InfoSubContainer>
+                </Community.InfoContainer>
+                <Community.InfoContainer>
+                    <Community.InfoContainerLabel>Filter</Community.InfoContainerLabel>
+                    <Toggle typo1="Daily" typo2="Schedule" element1={<Community.DailyFilter />} element2={<Community.ScheduleFilter />} />
+                    <div style={{ display: "flex", justifyContent: "center" }}>
+                        <Default.StyledLink to="/myPage"><Button typo="Sign Up" /></Default.StyledLink>
+                    </div>
+                </Community.InfoContainer>
+            </Styled.Container>
+        </div>
     )
 }
 
--- front/src/Pages/community/MyPage/Styled.js
+++ front/src/Pages/community/MyPage/Styled.js
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
+width: 668px;
+box-sizing: border-box;
+padding: 30px;
+margin: auto
+`
 
+export const Info = styled.span`
+height: 40px;
+display: flex;
+align-items: center;
+box-sizing: border-box;
+margin: 10px 0;
+padding: 0 10px;
+font-size: 18px;
+border-bottom: solid 1px #333;
 `
