81388d740bf7fefff2fe3a7c915ffa8ae2592ef9
xi_jjun <rlawowns97@naver.com>
2022-07-01 18:48:26 +0900

    [수정(Problem.java)] DB table 설계 변경에 따른 Entity변경
    본 프로젝트에서는 백준의 모든 문제에 대한 정보를 프로젝트가 구동되는 서버에 저장을 한 뒤에 관리를 할 것이다. 따라서 Problem table이 필요한 것이다.
    
    id : 백준 문제번호. 직접 DB에 값을 넣어줄 것이기 때문에 @GeneratedValue 를 사용하지 않았다.
    title : 문제 이름. KOR
    level : 문제 난이도. solved.ac에서 제공하는 티어표 기준에 따라 0~30까지로 나뉜다.
    tags : 문제 유형들에 대한 정보. ‘,’로 구분하여 각 유형정보를 표현할 수 있다.
    	ex) DFS, BFS, Implementation
    answerRate : 해당 문제에 대한 정답률 정보.
    problemUrl : 해당 문제가 존재하는 실제 문제 링크.

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/entity/Problem.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/entity/Problem.java
@@ -1,37 +1,17 @@
 package com.khk.backjoonrecommender.entity;
 
 import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-import java.time.LocalDateTime;
-import java.util.List;
 
 @Entity
 public class Problem {
 
     @Id
-    @GeneratedValue
     private Long id;
 
+    private String title;
     private int level;
-
-    private String type;
-
-    @Enumerated(value = EnumType.STRING)
-    private SolveType isSolved;
-
-    private LocalDateTime solvedDate;
-
-    @OneToMany(mappedBy = "problem", fetch = FetchType.LAZY)
-    private List<ProblemType> problemTypes;
-
-    @ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id")
-    private User user;
+    private String tags;
+    private Double answerRate;
+    private String problemUrl;
 }
