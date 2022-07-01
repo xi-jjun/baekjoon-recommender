04799fbc178286a856d7d8e311b22d95b8628337
nyk <nykoh2001@gmail.com>
2022-07-01 22:18:52 +0900

    [수정] Pagination 임포트

--- front/src/Pages/main/Solved/Solved.js
+++ front/src/Pages/main/Solved/Solved.js
@@ -1,46 +1,10 @@
 import "../../../default.css";
 import * as Styled from './Styled';
 import Header from "../../../Components/Header";
+import Pagination from "../../../Components/Pagination";
 import { useState } from 'react';
 
 const pages = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
-const Pagination = ({ number }) => {
-    const [pageClicked, pageOnClick] = useState(false)
-    const clickPage = () => {
-        pageOnClick(prev => !prev)
-    }
-
-    const [onMouseOver, setMouseOver] = useState(false)
-    const getMouseOver = () => {
-        setMouseOver(true)
-    }
-    const getMouseOut = () => {
-        setMouseOver(false)
-    }
-
-    return (
-        <div style={{
-            width: "30px",
-            height: "30px",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            background: pageClicked || onMouseOver ? "#0083e8" : "#fff",
-            boxSizing: "border-box",
-            borderRight: "solid 1px #0083e8",
-            color: pageClicked || onMouseOver ? "#fff" : "#0083e8",
-            fontWeight: pageClicked || onMouseOver ? 600 : 400,
-            cursor: "pointer",
-            transition: "all 0.1s ease-out"
-        }}
-            onClick={clickPage}
-            onMouseOver={getMouseOver}
-            onMouseOut={getMouseOut}>
-            {number}
-        </div>
-    )
-}
 
 const Solved = () => {
     return (
