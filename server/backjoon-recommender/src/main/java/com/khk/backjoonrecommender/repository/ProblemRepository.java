6e375d3e4b0f9fab1fbd99ee7f6b9f40ebdb3489
xi_jjun <rlawowns97@naver.com>
2022-07-01 19:12:27 +0900

    [추가] Problem을 title로 검색하기 위한 findByTitle method 추가

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/repository/ProblemRepository.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/repository/ProblemRepository.java
@@ -1,11 +1,10 @@
 package com.khk.backjoonrecommender.repository;
 
 import com.khk.backjoonrecommender.entity.Problem;
-import com.khk.backjoonrecommender.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
-	List<Problem> findByUser(User user);
+	Problem findByTitle(String title);
 }
