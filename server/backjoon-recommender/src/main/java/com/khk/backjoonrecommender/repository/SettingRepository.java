d205c37c6aa16b5c6e2bb8f98aeeb360ba053968
xi_jjun <rlawowns97@naver.com>
2022-07-03 17:08:00 +0900

    [수정(SettingRepository.java)] findByUser method 삭제

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/repository/SettingRepository.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/repository/SettingRepository.java
@@ -1,9 +1,7 @@
 package com.khk.backjoonrecommender.repository;
 
 import com.khk.backjoonrecommender.entity.Setting;
-import com.khk.backjoonrecommender.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface SettingRepository extends JpaRepository<Setting, Long> {
-	Setting findByUser(User user);
 }
