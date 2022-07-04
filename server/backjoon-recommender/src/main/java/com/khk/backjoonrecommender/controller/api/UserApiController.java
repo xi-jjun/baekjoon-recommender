8f4ed6194f8de9d5dd080466ace9d8bd2ea9dfc7
xi_jjun <rlawowns97@naver.com>
2022-07-05 00:23:15 +0900

    [수정] UserApiController method 수정

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/controller/api/UserApiController.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/controller/api/UserApiController.java
@@ -1,30 +1,33 @@
 package com.khk.backjoonrecommender.controller.api;
 
+import com.khk.backjoonrecommender.controller.dto.request.SignUpRequestDTO;
 import com.khk.backjoonrecommender.controller.dto.response.BasicResponseDto;
 import com.khk.backjoonrecommender.controller.dto.response.MyPageResponseDto;
 import com.khk.backjoonrecommender.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/user")
 @RestController
 public class UserApiController {
 
-    private final UserService userService;
+	private final UserService userService;
 
-    @GetMapping
-    public BasicResponseDto<MyPageResponseDto> userDetails() {
-        return userService.findUser();
-    }
+	@GetMapping
+	public BasicResponseDto<MyPageResponseDto> userDetails() {
+		return userService.findUser();
+	}
 
-    @PostMapping
-    public BasicResponseDto<?> userRegister() {
-        return userService.registerUser();
-    }
+	@PostMapping
+	public BasicResponseDto<?> userRegister(@RequestBody SignUpRequestDTO signUpRequestDTO) throws IOException {
+		return userService.registerUser(signUpRequestDTO);
+	}
 
-    @PatchMapping
-    public BasicResponseDto<?> userModify() {
-        return userService.modifyUser();
-    }
+	@PatchMapping
+	public BasicResponseDto<?> userModify() {
+		return userService.modifyUser();
+	}
 }
