eaee5a05f5286a883685f96227604e1b54e01232
xi_jjun <rlawowns97@naver.com>
2022-07-05 00:45:49 +0900

    [추가] UserServie register method 구현
    BasicUserService.java
    registerUser(SignUpRequestDTO signUpRequestDTO) method 기능 구현
    1. wrapper dto class인 signUpRequestDTO에서 user, setting dto를 꺼낸다.
    2. 해당 요청이 적절한지 검증을 한다.
    	1) 사용자가 입력한 baekJoonId가 실제로 백준 사이트에 가입된 계정인지 확인. => ValidationService에 위임.
    	2) 사용자가 입력한 정보가 플랫폼 정책을 준수하는지 확인. => ValidationService에 위임
    3. dto 를 entity로 바꾼다음 repository에 저장해준다. 저장할 때 password는 암호화를 해준다.

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/service/UserService.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/service/UserService.java
@@ -1,12 +1,15 @@
 package com.khk.backjoonrecommender.service;
 
+import com.khk.backjoonrecommender.controller.dto.request.SignUpRequestDTO;
 import com.khk.backjoonrecommender.controller.dto.response.BasicResponseDto;
 import com.khk.backjoonrecommender.controller.dto.response.MyPageResponseDto;
+
+import java.io.IOException;
 
 public interface UserService {
 	public BasicResponseDto<MyPageResponseDto> findUser();
 
-	public BasicResponseDto<?> registerUser();
+	public BasicResponseDto<?> registerUser(SignUpRequestDTO signUpRequestDTO) throws IOException;
 
 	public BasicResponseDto<?> modifyUser();
 }
