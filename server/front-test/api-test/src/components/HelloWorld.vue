539862714b16f472bce21667ba9908d44ed3e4eb
xi_jjun <rlawowns97@naver.com>
2022-07-07 00:40:22 +0900

    [TEST] 회원가입은 vue로 안되는 버그를 해결 모든 test를 vue에서 가능

--- server/front-test/api-test/src/components/HelloWorld.vue
+++ server/front-test/api-test/src/components/HelloWorld.vue
@@ -1,34 +1,10 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    
+    <h1>API TEST</h1>
 
-    <button id="join--submit" v-on:click="makeOneUser" class="mt-0 btn btn-secondary">SIGN UP - 현재 버그 있어서 html 파일에서 직접 해주세요</button>
+    <button id="join--submit" v-on:click="makeOneUser" class="mt-0 btn btn-secondary">SIGN UP</button>
     <button id="login-btn" v-on:click="login" class="mt-0 btn btn-secondary">login</button>
     <button id="get-user-info" v-on:click="findUser" class="mt-0 btn btn-secondary">Get user info test btn</button>
   </div>
@@ -78,9 +54,9 @@ export default {
       axios({
         method: 'post',
         url: 'http://localhost:8080/api/v1/user',
-        data: JSON.stringify(signUpRequestDTO),
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json'
+        data: signUpRequestDTO,
+      }).then(function (response) {
+        console.log(response);
       })
     },
     login() {
