317f7d6e5a96c529de2af885c63f6cb7c97445b2
nyk <nykoh2001@gmail.com>
2022-07-09 13:40:27 +0900

    [수정] 토큰 가져오기

--- front/src/shared/App.js
+++ front/src/shared/App.js
@@ -1,5 +1,5 @@
 import { React } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import Login from '../Pages/community/Login/Login';
 import SignUp from '../Pages/community/SignUp/SignUp';
 import Recommend from '../Pages/main/Recommend/Recommend';
@@ -7,15 +7,24 @@ import Rival from "../Pages/main/Rival/Rival";
 import Solved from "../Pages/main/Solved/Solved";
 import MyPage from '../Pages/community/MyPage/MyPage';
 
+const getToken = () => {
+    const tokenString = localStorage.getItem("Authorization")
+    const userToken = JSON.parse(tokenString)
+    return userToken?.token
+}
+
 function App() {
+    const token = getToken()
+    console.log("token: ", token)
+
     return (
         <Routes>
-            <Route path="/" element={<Login />} />
-            <Route path="/signUp" element={<SignUp />} />
-            <Route path="/recommend" element={<Recommend />} />
-            <Route path="/solved" element={<Solved />} />
-            <Route path="/rival" element={<Rival />} />
-            <Route path="/myPage" element={<MyPage />} />
+            <Route path="/" element={token ? <Recommend /> : <Navigate to="/user/login" />} />
+            <Route path="/user/login" element={<Login />} />
+            <Route path="/user/register" element={<SignUp />} />
+            <Route path="/user/my-page" element={token ? <MyPage /> : <Navigate to="/user/login" />} />
+            <Route path="/solved" element={token ? <Solved /> : <Navigate to="/user/login" />} />
+            <Route path="/rival" element={token ? <Rival /> : <Navigate to="/user/login" />} />
         </Routes>
     );
 }
