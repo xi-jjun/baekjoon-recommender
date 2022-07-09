1838bc9a7d2b323aee84c9157aa9deedc71e22ab
nyk <nykoh2001@gmail.com>
2022-07-09 13:40:46 +0900

    [수정] 회원가입 데이터 post

--- front/src/Pages/community/SignUp/SignUp.js
+++ front/src/Pages/community/SignUp/SignUp.js
@@ -22,16 +22,75 @@ function SignUp() {
         setBaekjoonId(e.target.value)
     }
 
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         const res = await axios.post("/user/register", {
-    //             username,
-    //             password,
-    //             option
-    //         })
-    //     }
-    //     getData()
-    // })
+    const trySignUp = (e) => {
+        const userRequestDTO = {
+            username: id,
+            baekJoonId: baekjoonId,
+            password: pw
+        };
+
+        const levels = [];
+        for (let i = 0; i < 31; i++) {
+            if (document.querySelector(`#sign-up-${i}`).checked) {
+                levels.push(i);
+            }
+        }
+
+        const tags = []
+        for (let e of document.querySelectorAll("#sign-up-daily-element")) {
+            if (e.checked) {
+                tags.push(e.getAttribute("value"))
+            }
+        }
+
+        const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
+        const daysSelected = {
+            mon: [],
+            tue: [],
+            wed: [],
+            thu: [],
+            fri: [],
+            sat: [],
+            sun: []
+        }
+        for (let day of days) {
+            const docu = document.querySelector(`#sign-up-schedule-${day}`)
+            if (docu && docu.checked) {
+                const elements = document.querySelectorAll(`#sign-up-schedule-element-${day}`)
+                for (let e of elements) {
+                    if (e.checked) {
+                        daysSelected[day].push(e.getAttribute("value"))
+                    }
+                }
+            }
+        }
+
+
+        const settingRequestDTO = {
+            option: document.querySelector("#sign-up-toggle").getAttribute("option"),
+            levels: levels.join(),
+            tags: tags.join(),
+            sun: daysSelected["sun"].join(),
+            mon: daysSelected["mon"].join(),
+            tue: daysSelected["tue"].join(),
+            wed: daysSelected["wed"].join(),
+            thu: daysSelected["thu"].join(),
+            fri: daysSelected["fri"].join(),
+            sat: daysSelected["sat"].join()
+        };
+
+        const signUpRequestDTO = {
+            userRequestDTO: userRequestDTO,
+            settingRequestDTO: settingRequestDTO
+        };
+
+        axios.post('http://localhost:8080/api/v1/user', signUpRequestDTO)
+            .then(res => {
+                console.log("data: ", res.data);
+                console.log("sign up success");
+            })
+            .catch(e => console.log("err: ", e));
+    }
 
     return (
         <Styled.Container>
@@ -54,13 +113,14 @@ function SignUp() {
                     <Community.InfoLabel>BaekJoon ID</Community.InfoLabel>
                     <Community.ShortInput placeholder='백준 id' id="baekjoon_id" type="text" onChange={handleInputBaekjoonId} />
                 </Community.InfoSubContainer>
-                <Styled.BaekjoonLink to={{ pathname: "https://www.acmicpc.net/" }} target="_blank">baekjoon online judge 바로 가기</Styled.BaekjoonLink>
+                <Styled.BaekjoonLink replace to={{ pathname: "https://www.acmicpc.net/" }} target="_blank">baekjoon online judge 바로 가기</Styled.BaekjoonLink>
             </Community.InfoContainer>
             <Community.InfoContainer>
                 <Community.InfoContainerLabel>Filter</Community.InfoContainerLabel>
-                <Toggle typo1="Daily" typo2="Schedule" element1={<Community.DailyFilter />} element2={<Community.ScheduleFilter />} />
+                <Community.DifficultyFilter />
+                <Toggle id="sign-up-toggle" typo1="Daily" typo2="Schedule" element1={<Community.DailyFilter id="sign-up-daily" />} element2={<Community.ScheduleFilter id="sign-up-schedule" />} />
             </Community.InfoContainer>
-            <Default.StyledLink to="/recommend"><Button typo="Sign Up" /></Default.StyledLink>
+            <Default.StyledLink to="/" onClick={trySignUp}><Button typo="Sign Up" /></Default.StyledLink>
         </Styled.Container>
     )
 }
