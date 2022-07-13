1642ad79b79c184235362ae7d88c99632c4364d2
nyk <nykoh2001@gmail.com>
2022-07-13 21:49:58 +0900

    [수정] modifyUser, 입력된 데이터 가져와서 체크 상태로 만들기 (미완)

--- front/src/Pages/community/MyPage/MyPage.js
+++ front/src/Pages/community/MyPage/MyPage.js
@@ -2,14 +2,18 @@ import Header from "../../../Components/Header";
 import * as Default from "../../../Default";
 import * as Community from "../Community";
 import * as Styled from "./Styled";
-import CommunityButton from "../../../Components/CommunityButton";
 import Toggle from "../../../Components/Toggle";
 import Button from "../../../Components/Button";
 import { useEffect, useState } from "react";
 import axios from "axios";
 
 const MyPage = () => {
-    const [ID, setId] = useState("")
+
+    const levels = [];
+    const tags = [];
+
+    const [data, setData] = useState(null);
+    const [baekjoonID, setBaekjoonId] = useState("")
     const [existingPw, setExistingPw] = useState("")
     const [newPw, setNewPw] = useState("")
 
@@ -20,19 +24,102 @@ const MyPage = () => {
         setNewPw(e.target.value)
     }
     const handleInputId = (e) => {
-        setId(e.target.value)
+        setBaekjoonId(e.target.value)
     }
 
-    // useEffect(() => {
-    //     const getData = async () => {
-    //         const res = await axios.patch("/user/my-page", {
-    //             username,
-    //             password,
-    //             option
-    //         })
-    //     }
-    //     getData()
-    // })
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/v1/user", {
+            headers: {
+                'Content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+                'Authorization': localStorage.getItem("Authorization")
+            }
+        }).then(res => {
+            setData(res.data.data);
+            console.log(data);
+            const levels = data.levels.split(",");
+            const dailyTags = data.dailyTags.split(",");
+            for (let l of levels) {
+                document.querySelector(`#my-page-${l}`).checked = true;
+            }
+            for (let t of dailyTags) {
+                for (let e of document.querySelectorAll("#my-page-type-element")) {
+                    if (e.typo == t) {
+                        e.checked = true;
+                    }
+                }
+            }
+        }).catch(e => {
+            console.log("error: ", e);
+        })
+    }, [])
+
+    const tryConfirm = (e) => {
+
+        e.preventDefault();
+        for (let i = 0; i < 31; i++) {
+            if (document.querySelector(`#my-page-${i}`).checked) {
+                levels.push(i);
+            }
+        }
+
+        for (let e of document.querySelectorAll("#my-page-daily-element")) {
+            if (e.checked) {
+                tags.push(e.getAttribute("value"))
+            }
+        }
+
+        const days = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+        const daysSelected = {
+            mon: [],
+            tue: [],
+            wed: [],
+            thu: [],
+            fri: [],
+            sat: [],
+            sun: []
+        };
+
+        for (let day of days) {
+            const docu = document.querySelector(`#my-page-schedule-${day}`)
+            if (docu && docu.checked) {
+                const elements = document.querySelectorAll(`#my-page-schedule-element-${day}`)
+                for (let e of elements) {
+                    if (e.checked) {
+                        daysSelected[day].push(e.getAttribute("value"))
+                    }
+                }
+            }
+        }
+
+        const signUpRequestDTO = {
+            username: data.username,
+            baekJoonId: baekjoonID,
+            password: newPw,
+            option: document.querySelector("#my-page-toggle").getAttribute("option"),
+            levels: levels.join(),
+            tags: tags.join(),
+            sun: daysSelected["sun"].join(),
+            mon: daysSelected["mon"].join(),
+            tue: daysSelected["tue"].join(),
+            wed: daysSelected["wed"].join(),
+            thu: daysSelected["thu"].join(),
+            fri: daysSelected["fri"].join(),
+            sat: daysSelected["sat"].join()
+        };
+
+        console.log("sign up request: ", signUpRequestDTO);
+
+        axios.patch("http://localhost:8080/api/v1/user", {
+            headers: {
+                "Authorization": localStorage.getItem("Authorization")
+            }
+        }, signUpRequestDTO)
+            .then(res => {
+                console.log("res: ", res);
+            }).catch(e => {
+                console.log("err: ", e);
+            })
+    }
 
     return (
         <div>
@@ -41,26 +128,24 @@ const MyPage = () => {
                 <Community.InfoContainer>
                     <Community.InfoContainerLabel>Membership Info</Community.InfoContainerLabel>
                     <Community.InfoSubContainer>
-                        <Community.InfoLabel>ID</Community.InfoLabel>
-                        <Community.ShortInput placeholder="새로운 ID" onChange={handleInputId} />
-                        <CommunityButton typo="수정" />
-                    </Community.InfoSubContainer>
-                    <Community.InfoSubContainer>
                         <Community.InfoLabel>PW</Community.InfoLabel>
                         <Community.Input placeholder="기존 pw" onChange={handleInputExistingPw} />
                     </Community.InfoSubContainer>
                     <Community.InfoSubContainer>
                         <Community.InfoLabel></Community.InfoLabel>
-                        <Community.ShortInput placeholder="새로운 pw" onChange={handleInputNewPw} />
-                        <CommunityButton typo="수정" />
+                        <Community.Input placeholder="새로운 pw" onChange={handleInputNewPw} />
+                    </Community.InfoSubContainer>
+                    <Community.InfoSubContainer>
+                        <Community.InfoLabel>Baekjoon ID</Community.InfoLabel>
+                        <Community.ShortInput placeholder="새로운 Baekjoon ID" onChange={handleInputId} />
                     </Community.InfoSubContainer>
                 </Community.InfoContainer>
                 <Community.InfoContainer>
                     <Community.InfoContainerLabel>Filter</Community.InfoContainerLabel>
-                    <Community.DifficultyFilter />
-                    <Toggle typo1="Daily" typo2="Schedule" element1={<Community.DailyFilter />} element2={<Community.ScheduleFilter />} />
+                    <Community.DifficultyFilter page="my-page" />
+                    <Toggle id="my-page-toggle" typo1="Daily" typo2="Schedule" element1={<Community.DailyFilter id="my-page-daily" />} element2={<Community.ScheduleFilter id="my-page-schedule" />} />
                     <div style={{ display: "flex", justifyContent: "center" }}>
-                        <Default.StyledLink to="/recommend"><Button typo="Confirm" /></Default.StyledLink>
+                        <Default.StyledLink to="/" onClick={tryConfirm}><Button typo="Confirm" /></Default.StyledLink>
                     </div>
                 </Community.InfoContainer>
             </Styled.Container>
