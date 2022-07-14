34e84c90fe4dbd95e7f7e86cc44c4ffe155b8f46
xi_jjun <rlawowns97@naver.com>
2022-07-14 17:44:02 +0900

    [수정] 스케쥴러 오류나서 일단 @Component 주석처리

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/common/ScheduledTasks.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/common/ScheduledTasks.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @RequiredArgsConstructor
-@Component
+//@Component
 public class ScheduledTasks {
 
         private final SystemService systemService;
