9ef498eeeb387dbfc594a9161d1f4d05fe48c4d0
xi_jjun <rlawowns97@naver.com>
2022-07-22 03:51:59 +0900

    [수정] Dto에서 삭제된 field들은 builder에서 제외하고 생성

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/controller/dto/request/UserRegisterRequestDto.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/controller/dto/request/UserRegisterRequestDto.java
@@ -19,13 +19,6 @@ public class UserRegisterRequestDto {
     private Option option;
     private String levels;
     private String tags;
-    private String sun;
-    private String mon;
-    private String tue;
-    private String wed;
-    private String thu;
-    private String fri;
-    private String sat;
 
     public UserRequestDto toUserDto() {
         return UserRequestDto.builder()
@@ -40,7 +33,6 @@ public class UserRegisterRequestDto {
                 .option(option)
                 .levels(levels)
                 .tags(tags)
-                .sun(sun).mon(mon).tue(tue).wed(wed).thu(thu).fri(fri).sat(sat)
                 .build();
     }
 }
