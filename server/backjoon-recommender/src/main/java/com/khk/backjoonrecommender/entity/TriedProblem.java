b5d1db0af1d63bb4c0f470e55a3a517129c55499
xi_jjun <rlawowns97@naver.com>
2022-07-22 01:44:47 +0900

    [수정] TriedProblem에서 필요없는 메서드 삭제

--- server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/entity/TriedProblem.java
+++ server/backjoon-recommender/src/main/java/com/khk/backjoonrecommender/entity/TriedProblem.java
@@ -22,7 +22,7 @@ public class TriedProblem {
 	@ManyToOne(targetEntity = User.class, fetch = FetchType.LAZY)
 	private User user;
 
-	@ManyToOne(targetEntity = Problem.class, fetch = FetchType.EAGER)
+	@ManyToOne(targetEntity = Problem.class, fetch = FetchType.LAZY)
 	private Problem problem;
 
 	@Enumerated(EnumType.STRING)
@@ -30,21 +30,8 @@ public class TriedProblem {
 	private LocalDateTime solvedDate;
 	private LocalDate recommendedDate;
 
-	public boolean solved() {
-		return this.solvingStatus.equals(SolvingStatus.PASS);
-	}
-
-	public boolean isRecommendedToday() {
-		final LocalDate TODAY = LocalDate.now();
-		return TODAY.equals(this.recommendedDate);
-	}
-
 	public boolean solving() {
 		return this.solvingStatus.equals(SolvingStatus.SOLVING);
-	}
-
-	public boolean isSameProblem(Long problemId) {
-		return this.getProblem().getId().equals(problemId);
 	}
 
 	public void updateSolvedStatus(SolvingStatus solvingStatus) {
