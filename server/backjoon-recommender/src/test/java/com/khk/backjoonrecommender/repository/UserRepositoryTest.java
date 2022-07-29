2dfaaddf10782ad6ef24b476f9bf633bc81d126f
xi_jjun <rlawowns97@naver.com>
2022-07-30 01:25:02 +0900

    [TEST] 사용자 정보 조회, 동일 username 중복 테스트 수정

--- server/backjoon-recommender/src/test/java/com/khk/backjoonrecommender/repository/UserRepositoryTest.java
+++ server/backjoon-recommender/src/test/java/com/khk/backjoonrecommender/repository/UserRepositoryTest.java
@@ -19,13 +19,21 @@ class UserRepositoryTest {
 
 	@Test
 	void successToSave() {
+		// given : 회원가입될 사용자 정보
 		final String USERNAME = "kjj97";
 		final String PASSWORD = "123";
 		final String BAEKJOON_ID = "bj000";
-		User user = new User(USERNAME, PASSWORD, BAEKJOON_ID);
+		User user = User.builder()
+				.baekJoonId(BAEKJOON_ID)
+				.username(USERNAME)
+				.password(PASSWORD)
+				.build();
 		userRepository.save(user);
 
+		// when : DB 에서 아까 저장된 사용자 정보를 조회할 때
 		User findUser = userRepository.findByUsername(USERNAME);
+
+		// then : 동일한 username 을 가져야 한다.
 		String findUsername = findUser.getUsername();
 		assertThat(findUsername).isEqualTo(USERNAME);
 	}
@@ -40,9 +48,8 @@ class UserRepositoryTest {
 		User sameUsernameUser = new User(SAME_USERNAME, PASSWORD, BAEKJOON_ID);
 
 		userRepository.save(user);
-		userRepository.save(sameUsernameUser);
 
-		assertThatThrownBy(() -> userRepository.flush())
+		assertThatThrownBy(() -> userRepository.save(sameUsernameUser))
 				.isInstanceOf(DataIntegrityViolationException.class);
 	}
 }
